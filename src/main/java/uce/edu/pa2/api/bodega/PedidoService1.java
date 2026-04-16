9cbdb6108e7ba2cdc4da83ed60f29d1a4f7a54ca
Lizzpulupaxz <lizlizzpulupa9@gmail.com>
2026-04-16 14:51:04 -0500

    Delete src/main/java/uce/edu/pa2/api/bodega/PedidoService1.java

--- src/main/java/uce/edu/pa2/api/bodega/PedidoService1.java
+++
@@ -1,9 +0,0 @@
-package uce.edu.pa2.api.bodega;
-
-public class PedidoService1 {
-    public void registrar(Pedido pedido) {
-        // Lógica para registrar el pedido
-        System.out.println("registrando pedido");
-
-    }
-}
